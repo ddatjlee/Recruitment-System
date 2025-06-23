d8d68185b9392d3702975110f7df65f21d187e3d
hungvlt <hungvu01235@gmail.com>
2025-06-23 11:26:01 +0700

    Thêm chức năng chia sẻ kinh nghiệm cá nhân của cựu thực tập sinh

--- DoAn_Web/Controllers/RecruiterController.cs
+++ DoAn_Web/Controllers/RecruiterController.cs
@@ -3,3 +3,4 @@ thêm chức năng tìm kiếm hồ sơ
 thêm chức năng nộp đơn ứng tuyển vào vị trí phù hợp của ứng viên
 thêm chức năng gửi thư thông báo từ NTD qua email của ứng viên
 thêm chức năng đăng nhập của cựu thực tập sinh
+thêm chức năng chia sẻ kinh nghiệm cá nhân của cựu thực tập sinh
